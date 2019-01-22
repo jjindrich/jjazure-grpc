1ad9bb1941f83cb39a9838f3806fc7e3068d48d6
Jaroslav Jindrich <jajindri@microsoft.com>
2019-01-22 16:12:18 +0100

    server - basic

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# jjazure-grpc
+# JJAzure gRCP service running on Azure
 Playground for gRPC in Azure
+
+## Create gRPC server
+
+I will create new server running service called JJTaskManager.
+
+Create new dotNet Core project and follow this steps:
+- add nuget Grpc, Grpc.Tools, Google.Protobuf
+- define gRPC service and messages (jjtask.proto)
+- add code to run server
---
+++ jjgrpcs-server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using JJTaskGrpcDemo;
+
+namespace jjgrpcs_server
+{
+    class Program
+    {
+
+        class JJTaskManagerImpl : JJTaskManager.JJTaskManagerBase
+        {
+            public override Task<JJTaskDetail> AddTask(JJTask task, ServerCallContext context)
+            {
+                return Task.FromResult(new JJTaskDetail { Id = 100, Name = "New task" });
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const int Port = 8080;
+
+            Server server = new Server
+            {
+                Services = { JJTaskManager.BindService(new JJTaskManagerImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("JJTaskManager service is listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
